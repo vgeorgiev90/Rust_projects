97f48a015b2407d410bf1cc19612ab1d9cab5328
Viktor Georgiev <viktor@cobrowser.net>
2024-11-17 14:30:35 +0200

    port_scanner - 0.2.0
    Adding an update for the port scanner, async ping sweep and scan trough tokio

--- port_scanner/src/main.rs
+++ port_scanner/src/main.rs
@@ -1,9 +1,10 @@
 mod scanner;
 mod utils;
-use std::thread;
-use std::sync::mpsc;
+use tokio::sync::mpsc;
+use tokio::task;
+use surge_ping::{Client, Config};
 extern crate argparse;
-use argparse::{ArgumentParser, Store};
+use argparse::{ArgumentParser, Store, StoreTrue};
 use std::time::Instant;
 use colored::*;
 
@@ -13,11 +14,14 @@ TODO
 2. Possibly try to grab a banner if it exist
 */
 
-fn main() {
+//#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 8)]
+async fn main() {
 
-    let mut threads_arg: String = "".to_string();
+    let mut threads_arg: String = "30".to_string();
     let mut hosts: String = "".to_string();
     let mut range_arg: String = "".to_string();
+    let mut no_ping: bool = false;
 
     //Argument parser
     {
@@ -28,12 +32,14 @@ fn main() {
         ap.refer(&mut range_arg)
             .add_option(&["-p", "--port-range"], Store, "Port range to scan, eg: 1000 to scan from 1-1000");
         ap.refer(&mut threads_arg)
-            .add_option(&["-t", "--threads"], Store, "Number of threads to run the scan with");
+            .add_option(&["-t", "--threads"], Store, "Number of threads to run the scan with, default: 30");
+        ap.refer(&mut no_ping)
+            .add_option(&["-n", "--no-ping"], StoreTrue, "Dont make a ping sweep to determine live hosts");
         ap.parse_args_or_exit();
     }
 
     //If there are no args passed exit
-    if range_arg.is_empty() || hosts.is_empty() || threads_arg.is_empty() {
+    if range_arg.is_empty() || hosts.is_empty() {
         println!("{}\n./port_scanner -h to see available options", "[!] No arguments passed".red());
         return;
     }
@@ -46,6 +52,7 @@ fn main() {
             return;
         }
     };
+
     let threads: u32 = match threads_arg.parse::<u32>() {
         Ok(thrads) => thrads,
         Err(_e) => {
@@ -55,21 +62,63 @@ fn main() {
     };
 
     //Parse the range to scan
-    let range_to_scan: Vec<String> = utils::parse_hosts(hosts.as_str());
+    let mut range_to_scan: Vec<String> = utils::parse_hosts(hosts.as_str());
+
+
+    // Ping discovery
+    let mut live_hosts: Vec<String> = Vec::new();
+    if !no_ping {
+
+        println!("[+] Doing ping sweep on target: {}", hosts.blue());
+        //let (sender, mut receiver) = mpsc::unbounded_channel();
+        let (sender, mut receiver) = mpsc::channel(2000);
+
+        let chunk_size = (range_to_scan.len() + (threads as usize) - 1) / threads as usize;
+        let host_parts: Vec<Vec<String>> = range_to_scan.chunks(chunk_size).map(|c| c.to_vec()).collect();
+        let client_v4 = Client::new(&Config::default()).expect("[!] Could not create ICMP client");
+
+        let mut ping_tasks = Vec::new();
+        for part in host_parts {
+
+            let sender_clone = sender.clone();
+            let client_clone = client_v4.clone();
+    
+            let pinger = task::spawn(async move {
+                match scanner::ping_sweep(client_clone, part).await {
+                    Some(live_host) => sender_clone.send(live_host).await.unwrap(),
+                    None => {}
+                };
+            });
+    
+            ping_tasks.push(pinger);
+        }
+        drop(sender);
+    
+        while let Some(data) = receiver.recv().await {
+            live_hosts.extend(data);
+        }
+        // Wait for all tasks to finish
+        for task in ping_tasks {
+            task.await.unwrap();
+        }
+        range_to_scan = live_hosts;
+    }
+
 
     println!("[+] Total hosts to scan: {}", range_to_scan.len().to_string().yellow());
-    println!("[+] Scheduling {} ports scan to {} threads\n", range.to_string().yellow(), threads.to_string().yellow());
-
+    println!("[+] Scanning {} ports with {} threads\n", range.to_string().yellow(), threads.to_string().yellow());
     //Loop trough all hosts and do a port scan
     for host in range_to_scan {
         let start = Instant::now();
 
         //Channel for thread communication
-        let (sender, receiver) = mpsc::channel();
+        //let (sender, mut receiver) = mpsc::unbounded_channel();
+        let (sender, mut receiver) = mpsc::channel(2000);
+
         //Vector to store opened ports
         let mut opened_ports: Vec<Vec<u32>> = Vec::new();
         //Vector to store thread handles
-        let mut handles = Vec::new();
+        let mut tasks = Vec::new();
 
         println!("[+] Starting port scan for host: {}", host.blue());
 
@@ -85,30 +134,30 @@ fn main() {
             let host_clone = host.clone();
             //println!("[+] Scheduling thread {} for range: {}-{}", i, previous, current);
 
-            let handle = thread::spawn(move || {
-                let ports: Vec<u32> = scanner::scan_tcp_ports(previous, current, host_clone.as_str());
+            let scan_task = task::spawn(async move {
+                let ports: Vec<u32> = scanner::scan_tcp_ports(previous, current, host_clone.as_str()).await;
             
                 //If there are open ports send them to the main thread
                 if !ports.is_empty() {
-                    sender_clone.send(ports).expect("[!] Cant send open ports to main");
+                    sender_clone.send(ports).await.expect("[!] Cant send open ports to main");
                 }
             });
 
             //push the handle to the handles vector
-            handles.push(handle);
+            tasks.push(scan_task);
         }
         
         //Drop the sender after all threads are shceduled to prevent deadlocks in the receiver
         drop(sender);
 
         //Receive until there are no more senders
-        while let Ok(data) = receiver.recv() {
+        while let Some(data) = receiver.recv().await {
             opened_ports.push(data);
         }
 
         //Wait for thread completion
-        for hand in handles {
-            hand.join().expect("[!] Thread panicked...");
+        for task in tasks {
+            task.await.unwrap();
         }
 
         //Measure how much time each scan took.
@@ -119,7 +168,7 @@ fn main() {
             println!("[+] Scan duration: {:.2}s", duration.as_secs_f64());
             println!("[+] Opened ports: \n\n{}\n", opened_ports.concat().iter().map(|port| port.to_string()).collect::<Vec<String>>().join(", ").green());
         } else {
-            println!("[+] {}, most likely the host is not live\n", "No open ports".red());
+            println!("[+] {}, the host is not live, or connect attempts are being filtered.\n", "No open ports".red());
         }
     }
 }
--- port_scanner/src/scanner.rs
+++ port_scanner/src/scanner.rs
@@ -1,27 +1,73 @@
-use std::net::{TcpStream, Ipv4Addr, ToSocketAddrs};
+use surge_ping::{Client, PingIdentifier, PingSequence};
 use std::process;
-use std::time::Duration;
+use rand::random;
+use tokio::net::TcpStream;
+use tokio::time::{timeout, Duration};
+use std::net::{Ipv4Addr, ToSocketAddrs, IpAddr};
+
+
 
 const TIMEOUT: Duration = Duration::from_millis(500);
 
-pub fn scan_tcp_ports(previous: u32, current: u32, host: &str) -> Vec<u32> {
 
-    //Check if the provided ip is actually a valid IPv4 address
+
+pub async fn scan_tcp_ports(previous: u32, current: u32, host: &str) -> Vec<u32> {
     if !host.parse::<Ipv4Addr>().is_ok() {
         println!("[!] Not a valid IPv4 address");
         process::exit(1);
     }
 
-    let mut opened_ports: Vec<u32> = Vec::new();
-
+    let mut opened_ports = Vec::new();
     for port in previous..current {
         let sock: String = format!("{}:{}", host, port);
-        match TcpStream::connect_timeout(&sock.to_socket_addrs().unwrap().next().unwrap(), TIMEOUT) {
-            Ok(_) => {
+        let addr = sock.to_socket_addrs().unwrap().next().unwrap();
+
+        match timeout(TIMEOUT, TcpStream::connect(addr)).await {
+            Ok(Ok(_)) => {
                 opened_ports.push(port);
             },
-            Err(_e) => {}
+            Ok(Err(e)) => {
+                if e.kind() == std::io::ErrorKind::ConnectionRefused {
+                    // Closed port, no issue.
+                } else if e.kind() == std::io::ErrorKind::Other && e.to_string().contains("No route to host") {
+                    // Dead host, no point in scanning the whole range
+                    return opened_ports;
+                }
+            },
+            Err(_) => {
+                // Timeout occurred.
+            }
         }
     }
     return opened_ports;
+}
+
+
+
+pub async fn ping_sweep(client: Client, hosts: Vec<String>) -> Option<Vec<String>> {
+    
+    let mut live_hosts: Vec<String> = Vec::new();
+
+    for host in hosts {
+        let target = IpAddr::V4(host.parse::<Ipv4Addr>().unwrap());
+
+        let payload = [0; 56];
+        let mut ping_client = client.pinger(target, PingIdentifier(random())).await;
+        ping_client.timeout(std::time::Duration::from_secs(1));
+
+        match ping_client.ping(PingSequence(1), &payload).await {
+            Ok(_) => {
+                live_hosts.push(host);
+            },
+            Err(_e) => {
+                //Host is down   
+            }
+        }
+    }
+
+    if !live_hosts.is_empty() {
+        return Some(live_hosts);
+    } else {
+        return None;
+    }
 }
